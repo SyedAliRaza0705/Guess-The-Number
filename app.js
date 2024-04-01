421118a0f7535c671585cbe57df5cdedad286a35
SyedAliRaza0705 <164862312+SyedAliRaza0705@users.noreply.github.com>
2024-04-01 06:49:14 +0500

    Number Guessing Game

---
+++ app.js
@@ -0,0 +1,18 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+console.log("Guess a Number bbetween 1 to 10");
+let a = Math.floor(Math.random() * 10);
+while (true) {
+    let input = await inquirer.prompt({ name: "guess", type: "number",
+        message: "Enter your guess you want between 1 to 10; "
+    });
+    // provide code
+    let ans = input.guess;
+    if (a == ans) {
+        console.log("Congratulations your guess number is correct ");
+        break;
+    }
+    else {
+        console.log("Sorry you guess wrong number try again");
+    }
+}
